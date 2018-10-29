b9fa928dd1040db49eafd64667ff9897f60d66f7
Br1 <brunogiovagnoli@gmail.com>
2018-10-29 08:05:19 -0300

    deleted, just offline game i guess

--- paseahistoria/front-end/js/my-js/about.js
+++
@@ -1,9 +0,0 @@
-function showLicense() {
-    document.getElementById("licenceText").classList.remove("hidden");
-    document.getElementById("licenceText").classList.add("visible");
-}
-
-function hideLicense() {
-    document.getElementById("licenceText").classList.add("hidden");
-    document.getElementById("licenceText").classList.remove("visible");
-}
--- paseahistoria/history/chapters-class/prepareChapter.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start(); // Start the session
-
-    if ($_POST['info'] == "intro" || $_POST['info'] == "arg1810H" || $_POST['info'] == "arg1810" || $_POST['info'] == "arg1810M")
-    {
-        $_SESSION["chapterName"] = $_POST['info'];
-        $_SESSION["currentScreen"] = 0;
-    }
-?>
