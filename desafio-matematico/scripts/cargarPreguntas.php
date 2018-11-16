4193c87ee6c538fefd87e5e4eec3b0a9c56f0537
Br1 <brunogiovagnoli@gmail.com>
2018-11-15 21:19:15 -0300

    trying to connect to db

--- desafio-matematico/scripts/cargarPreguntas.php
+++ desafio-matematico/scripts/cargarPreguntas.php
@@ -1,6 +1,10 @@
 <?php
 $db_col = $_GET['db_col'];
-$con = mysqli_connect('localhost','root','','des_matematico');
+$DB_HOST = "us-cdbr-iron-east-01.cleardb.net";
+$DB_DATABASE = "heroku_89006519200a5c9";
+$DB_USERNAME = "b63c2ed4ddb4f3";
+$DB_PASSWORD = "00fec8f7";
+$con = mysqli_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
 if (!$con) {
     die('Imposible conectar a la base de datos: ' . mysqli_error($con));
 }
